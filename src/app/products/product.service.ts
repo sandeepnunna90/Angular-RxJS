5332572005deeaee73af1badf29435861397c9e7
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-08-04 22:39:16 -0400

    Cannot do this because, map is getting an product array with products - treats it as one element of the stream

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -18,6 +18,10 @@ export class ProductService {
 
   products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
+      // map(item => item.price * 1.5),
+      map(products =>
+        products.map(product => product.price * 1.5)
+      ),
       tap(data => console.log('Products: ', JSON.stringify(data))),
       // catch & rethrow strategy
       catchError(this.handleError)

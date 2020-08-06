d3220a397c71f1e88562d83248ed7a00cbf2786e
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-08-05 22:33:39 -0400

    use productWithCategory stream to in product alt component and update to display category name

--- src/app/products/product-list-alt/product-list-alt.component.ts
+++ src/app/products/product-list-alt/product-list-alt.component.ts
@@ -13,7 +13,7 @@ export class ProductListAltComponent {
   errorMessage = '';
   selectedProductId: number;
 
-  products$ = this.productService.products$
+  products$ = this.productService.productsWithCategory$
     .pipe(
       catchError(err => {
         this.errorMessage = err;

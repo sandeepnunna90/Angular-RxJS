82fefaafe14e345438c301394f47cc5fe3c8e8e4
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-08-05 22:40:27 -0400

    React to selection: data stream - product list component

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -42,6 +42,14 @@ export class ProductService {
     )
   );
 
+  selectedProduct$ = this.productsWithCategory$
+    .pipe(
+      map(products =>
+        products.find(product => product.id === 5)
+      ),
+      tap(product => console.log('selectedProduct', product))
+    );
+
   constructor(
     private http: HttpClient,
     private supplierService: SupplierService,

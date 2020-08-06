5aed70edfa5de8d39b5cf2d2fed1fb6bcd2274e2
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-08-06 00:21:49 -0400

    cache categories using shareReplay operator

--- src/app/product-categories/product-category.service.ts
+++ src/app/product-categories/product-category.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { throwError, Observable } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
+import { tap, catchError, shareReplay } from 'rxjs/operators';
 
 import { ProductCategory } from './product-category';
 
@@ -16,6 +16,7 @@ export class ProductCategoryService {
   productCategories$ = this.http.get<ProductCategory[]>(this.productCategoriesUrl)
     .pipe(
       tap(data => console.log('categories', JSON.stringify(data))),
+      shareReplay(1),
       catchError(this.handleError)
     );
 
